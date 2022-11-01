79d39df1c787227650c12c511c10cc73c9c9f44a
Divyansh pal <88077772+Divyansh-Pal@users.noreply.github.com>
2022-11-01 13:23:50 +0530

    Add files via upload

---
+++ nested.py
@@ -0,0 +1,12 @@
+#Made by Divyansh
+#WAP for creating nested loop
+
+count = 1
+for i in range(10):
+    print(str(i)*i)
+    for i in range (0,i):
+        count = count+1
+
+#Run and execute the code.        
+
+        
---
+++ si-calc.py
@@ -0,0 +1,13 @@
+#Made by Divyansh
+#WAP to find Simple Interest with net Amount
+
+print('Interest Calculation:')
+P = float(input('Pincipal?'))
+R = float(input('Rate of interest?'))
+T = float(input('Duration (No. of years)?'))
+
+Amount = (P*pow(1 + (R/100),T))
+Interest = Amount-P
+print('/nInterest = %0.2f'%Interest)
+
+#run and execute the code.
