f5661b8f55d16e3a976ab7f2a559347da575476e
Sumit <90320919+Sumitcoders@users.noreply.github.com>
2022-11-05 09:35:13 +0530

    @Sumitcoders

--- intrest finder.py
+++ intrest finder.py
@@ -1,6 +1,8 @@
+#WAP a code to find the simple interest of the given ammount of the user
+
 p = float(input('Enter principal amount: '))
 t = float(input('Enter the time (in years): '))
 r = float(input('Enter the rate of intrest: '))
 
 si = (p*t*r)/100
-print(f"The simple intrest is {si} rupees and the total amount is {si + p} rupees")
+print(f"The simple interest is {si} rupees and the total amount is {si + p} rupees")
