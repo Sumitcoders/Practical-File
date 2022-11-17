d431378a9b60eda06389882f0305e1df4c6dbe47
Divyansh pal <88077772+Divyansh-Pal@users.noreply.github.com>
2022-11-17 21:41:25 +0530

    Add files via upload

---
+++ dice-sim.py
@@ -0,0 +1,52 @@
+#Made by Divyansh
+#WAP a dice simulator
+
+import random
+
+rolled = random.randint(1,6)
+
+if rolled == 1:
+        print(" _____")
+        print("|     |")
+        print("|     |")
+        print("|  0  |")
+        print("|     |")
+        print("|_____|")
+if rolled == 2:
+        print(" _____")
+        print("|     |")
+        print("| 0   |")
+        print("|     |")
+        print("|   0 |")
+        print("|_____|")
+if rolled == 3:
+        print(" _____")
+        print("|     |")
+        print("|     |")
+        print("|0 0 0|")
+        print("|     |")
+        print("|_____|")
+if rolled == 4:
+        print(" _____")
+        print("|     |")
+        print("|0   0|")
+        print("|     |")
+        print("|0   0|")
+        print("|_____|")
+if rolled == 5:
+        print(" _____")
+        print("|     |")
+        print("|0   0|")
+        print("|  0  |")
+        print("|0   0|")
+        print("|_____|")
+if rolled == 6:
+        print(" _____")
+        print("|     |")
+        print("|0 0 0|")
+        print("|     |")
+        print("|0 0 0|")
+        print("|_____|")            
+
+
+#run and execute the code.
