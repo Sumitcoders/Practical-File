39d1e984ec2ea1a60a3ac55d3dc294b4d17c0acb
Divyansh pal <88077772+Divyansh-Pal@users.noreply.github.com>
2022-11-17 21:38:07 +0530

    Add files via upload

---
+++ conditions.py
@@ -0,0 +1,35 @@
+#Made by Divyansh
+#WAP to illustrate conditional statements in Python with comparison operators.
+
+A=100
+B=80
+if(A==B):
+    print("A is equal to B")
+else:
+    print("A is not equal to B")
+if(A!=B):
+    print("A is not equal to B")
+else:
+    print("A is equal to B")
+if(A<B):
+    print("A is less than B")
+else:
+    print("A is not less than B")
+if(A>B):
+    print("A is greater than B")
+else:
+    print("A is not greater than B")
+
+A=5
+B=20
+if(A<=B):
+    print("A is either less than or equal to B")
+else:
+    print("A is neither less than nor equal to B")
+if(A>=B):
+    print("A is either greater than or equal to B")
+else:
+    print("A is neither greater than nor equal to B")
+
+#run and execute the code.    
+    
