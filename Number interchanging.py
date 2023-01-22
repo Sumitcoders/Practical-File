031854a6c410c47e1044059ab90a10d55a9bfe42
VivekCoders <114720185+JustCodingWithVivek@users.noreply.github.com>
2023-01-22 15:28:02 +0530

    Add files via upload

---
+++ Number interchanging.py
@@ -0,0 +1,14 @@
+A=int(input("Enter First Number: "))
+B=int(input("Enter First Number: "))
+
+print("Numbers before changing")
+print("Number 1:",A)
+print("Number 2:",B) 
+print()
+C=A
+A=B
+B=C
+
+print("Numbers after changing")
+print("Number 1: ",A)
+print("Number 2: ",B)
