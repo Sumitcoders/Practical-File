f0c692b5281f3778b8168ed93b450acadf60f3cf
Your Daddy <97085312+Badal-Hasda@users.noreply.github.com>
2023-01-28 22:21:00 +0530

    Add files via upload

---
+++ beginners.py
@@ -0,0 +1,35 @@
+#Beginners helping program...
+
+print("Hello world..") # Print is used for executing..and for a string it must inside {""}..
+
+#Back to the basics.. bois
+
+# yeah bro Hastag is used for commenting out or for not running that specific code..
+
+
+#beginners adding program.. with predefined terms..
+
+x = 10
+y = 12
+
+m= x+y
+
+print(m)
+
+
+#beginners adding program with user given input..
+
+x2 = int(input("1st no : "))
+y2 = int(input("2nd no : "))
+
+m2 = x2+y2
+
+print(" The answer is : ",m2)
+
+#Here int() function is used for getting input of inetegral terms only...
+# And input() function is used for getting input from the user...
+#Also you already know {""} are used to print string.. 
+#String is a type because of which reading text can be printed / executed...
+
+
+# Hope you will learn something from here...
