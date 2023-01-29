caf04f2c9c4c6dd9eb4dbca326a506a781ab24a5
Divyansh pal <88077772+Divyansh-Pal@users.noreply.github.com>
2023-01-29 21:23:34 +0530

    Add files via upload

---
+++ convert.py
@@ -0,0 +1,8 @@
+#WAP to change celsius to Fahrenheit
+#made by Divyansh Pal
+
+celsius = float(input("Enter temperature in celsius: "))
+fahrenheit = (celsius * 1.8) + 32
+print(str(celsius )+ " degree Celsius is equal to " + str(fahrenheit )+ " degree Fahrenheit.")
+
+#run and execute.
