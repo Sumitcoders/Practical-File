29c69d0e26bf1a4fbe106c82b279ac9bebc1cc91
Divyansh pal <88077772+Divyansh-Pal@users.noreply.github.com>
2023-01-29 21:18:07 +0530

    @Divyansh pal

---
+++ test.py
@@ -0,0 +1,15 @@
+#WAP anything for exam practice
+
+a=int(input("Enter first number:\n"))
+b=int(input("Enter second number:\n"))
+c=int(input("Enter third number:\n"))
+if a>b:
+    if a>c:
+        print("The greatest no. here is:",a)
+if b>a:
+    if b>c:
+        print("The greatest no. here is:",b) 
+if c>a:
+    if c>b:
+        print("The greatest no. here is:",c)
+#run and execute.
